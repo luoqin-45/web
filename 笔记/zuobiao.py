1cb46482d972c09d716d4d242d695a26ebdd1d71
Qin Luo (NCS) <qin.luo@ncsi.com.cn>
2021-08-04 15:13:47 +0800

    html

--- 笔记/zuobiao.py
+++ 笔记/zuobiao.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 from PIL import Image
 
 
-img = Image.open('map.jpg')  # 读取图片
+img = Image.open('ico.jpg')  # 读取图片
 plt.imshow(img)
 # plt.xticks([]), plt.yticks([])  # 隐藏x轴和y轴的函数值
 plt.show()
